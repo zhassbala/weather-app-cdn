a13f13416b5a54c2e670b58d94d430449cb09b8b
Rassul Bessimbekov <besimbekovr@gmail.com>
2023-07-30 03:58:20 +0500

    compiled js&css file for cdn

---
+++ index.js
@@ -0,0 +1,9 @@
+var link = document.createElement("link");
+link.href = location.href + "assets/index-b753b33f.css";
+link.type = "text/css";
+link.rel = "stylesheet";
+link.media = "screen,print";
+
+document.getElementsByTagName("head")[0].appendChild(link);
+
+import "./index-e8206105.js";
