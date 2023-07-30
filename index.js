9994bdcebd88fc22cf139ebe975eabd68eb57c8f
Rassul Bessimbekov <besimbekovr@gmail.com>
2023-07-30 17:30:09 +0500

    change names of js&css files + updated js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const link = document.createElement("link");
-link.href =
-  "https://cdn.jsdelivr.net/gh/zhassbala/weather-app-cdn/index-b753b33f.css";
+link.href = "https://cdn.jsdelivr.net/gh/zhassbala/weather-app-cdn/index.css";
 link.type = "text/css";
 link.rel = "stylesheet";
 link.media = "screen,print";
 link.onload = () => {
-  import("./index-e8206105.js");
+  import("./app.min.js");
 };
 
 document.getElementsByTagName("head")[0].appendChild(link);
